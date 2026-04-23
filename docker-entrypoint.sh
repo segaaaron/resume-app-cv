12e45ad8f390b79e1c9dea3c36c846330f4f9de3
Miguel Angel Saravia <msaravia@ia.com.mx>
2026-04-23 14:07:03 -0400

    Fixed new errores at prima and dockerfile

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "▶ Running Prisma db push..."
-DATABASE_URL="$DATABASE_URL" node node_modules/prisma/build/index.js db push --skip-generate --accept-data-loss
+DATABASE_URL="$DATABASE_URL" node node_modules/prisma/build/index.js db push --accept-data-loss
 
 echo "▶ Starting Next.js..."
 exec node server.js
