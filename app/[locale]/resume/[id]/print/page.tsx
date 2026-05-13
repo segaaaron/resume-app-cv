e9084cf2a40548f1c50d961b1f5e4310c93e7f45
Miguel Angel Saravia <msaravia@ia.com.mx>
2026-05-13 18:35:30 -0400

    Fixed load pdf enginer

--- app/[locale]/resume/[id]/print/page.tsx
+++ app/[locale]/resume/[id]/print/page.tsx
@@ -7,6 +7,34 @@ import type { ResumeSection, ResumeSections, ResumeConfig } from "@/types/resume
 import { DEFAULT_SECTIONS, ResumeSectionsSchema } from "@/types/resume"
 import PrintLayout from "@/components/resume/PrintLayout"
 
+const FONT_FILES: Record<string, string[]> = {
+  Inter:              ["inter-400", "inter-600", "inter-700"],
+  Poppins:            ["poppins-400", "poppins-600", "poppins-700"],
+  Roboto:             ["roboto-400", "roboto-700"],
+  Lato:               ["lato-400", "lato-700"],
+  Montserrat:         ["montserrat-400", "montserrat-600", "montserrat-700"],
+  "Playfair Display": ["playfair-400", "playfair-700", "playfair-400-italic", "playfair-700-italic"],
+  Merriweather:       ["merriweather-400", "merriweather-700"],
+  "Open Sans":        ["open-sans-400", "open-sans-600", "open-sans-700"],
+  Raleway:            ["raleway-400", "raleway-600", "raleway-700"],
+  "Source Sans 3":    ["source-sans-400", "source-sans-600", "source-sans-700"],
+}
+
+function FontPreload({ fontFamily }: { fontFamily: string }) {
+  const files = [
+    ...(FONT_FILES["Inter"] ?? []),
+    ...(FONT_FILES[fontFamily] ?? []),
+  ].filter((v, i, a) => a.indexOf(v) === i)
+
+  return (
+    <>
+      {files.map((f) => (
+        <link key={f} rel="preload" as="font" type="font/woff2" href={`/fonts/${f}.woff2`} crossOrigin="anonymous" />
+      ))}
+    </>
+  )
+}
+
 export default async function PrintPage({
   params,
   searchParams,
@@ -58,13 +86,16 @@ export default async function PrintPage({
   )
 
   return (
-    <PrintLayout
-      resumeId={resume.id}
-      title={resume.title}
-      sections={sections}
-      sectionData={sectionData}
-      config={config}
-      isPro={isPro}
-    />
+    <>
+      <FontPreload fontFamily={config.fontFamily ?? "Poppins"} />
+      <PrintLayout
+        resumeId={resume.id}
+        title={resume.title}
+        sections={sections}
+        sectionData={sectionData}
+        config={config}
+        isPro={isPro}
+      />
+    </>
   )
 }
